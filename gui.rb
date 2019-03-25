e46a21b14f87cb8897deb443311bd9492c83e594
hadesnotatsujin <leopardsarekittens522@gmail.com>
2019-03-25 10:36:14 -0400

    Create gui.rb

---
+++ gui.rb
@@ -0,0 +1,8 @@
+  Shoes.app {
+    background "#F66".."#9BD"
+    title("osk-to-rb",
+          top:    60,
+          align:  "center",
+          font:   "Trebuchet MS",
+          stroke: white)
+  }
