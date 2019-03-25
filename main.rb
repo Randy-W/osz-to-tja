35e91cc7002fa9a061b4d3993e804ff0990b1ea3
Hades // James <leopardsarekittens522@gmail.com>
2019-03-25 15:46:10 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,10 @@
 # I will begin development on this soon. Until then, have a blank file!
+
+  Shoes.app {
+    background "#F66".."#9BD"
+    title(".osz  to .tja converter!",
+          top:    60,
+          align:  "center",
+          font:   "Trebuchet MS",
+          stroke: white)
+  }
