c5ddf20d0708a56bad8923d3a6dceb275b54beaa
Hades // James <leopardsarekittens522@gmail.com>
2019-03-25 15:45:58 -0400

    merging into main.rb

--- gui.rb
+++
@@ -1,8 +0,0 @@
-  Shoes.app {
-    background "#F66".."#9BD"
-    title(".osz  to .tja converter!",
-          top:    60,
-          align:  "center",
-          font:   "Trebuchet MS",
-          stroke: white)
-  }
