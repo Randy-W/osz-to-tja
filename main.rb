8139a9467d490c75529a5256a42d8825abf404aa
Hades // James <leopardsarekittens522@gmail.com>
2019-03-26 10:57:32 -0400

    Add code for when I actually figure this out

--- main.rb
+++ main.rb
@@ -11,3 +11,13 @@ require 'Shoes'
           font:   "Trebuchet MS",
           stroke: white)
   }
+
+## When convert button is pressed
+# File.rename("*.osk", "*.zip")   #=> 0
+#  Zip::ZipFile.open(file_path) { |zip_file|
+#     zip_file.each { |f|
+#     f_path=File.join("destination_path", f.name)
+#     FileUtils.mkdir_p(File.dirname(f_path))
+#     zip_file.extract(f, f_path) unless File.exist?(f_path)
+#   }
+#  }
