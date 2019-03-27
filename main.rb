aaa375b50adb8562c644fa89ae9e9e1479c86883
Hades // James <leopardsarekittens522@gmail.com>
2019-03-27 07:49:30 -0400

    hopefully fix some changes

--- main.rb
+++ main.rb
@@ -22,28 +22,25 @@ end
 # --- END GUI --- # 
 
 #When convert button is pressed
-File.rename("*.osk$", "*.zip$")
+File.rename("*.osz", "*.zip$")
 
-dialog.directory(
+dialog.directory()
 
-    def extract_zip(file, destination) FileUtils.mkdir_p(destination)
+  def extract_zip(file, destination) FileUtils.mkdir_p(destination)
 
     file_path = "./convert_temp/*.zip"
     destination = "./convert_temp/osz/"
 
     extract_zip(file_path, destination)
-      
-until File.exists?( ".osu$" ) == false do
-  File.rename("./convert_temp/osz/*.osu$", "*.txt$")
-  File.foreach(filename) do |file|
-  
-  file_string = File.read('./convert_temp/osz/*.txt$')
 
-  if file_string.include?('Mode: 1')
-    puts 'Yes'
-  else
-    puts 'No'
-  end
-end
-end
+    until File.exists?( "*.osu" ) == false do
+      File.rename("./convert_temp/osz/*.osu", "./convert_temp/osz/*.txt")
+      File.foreach(filename) do |file_string|
+        if file_string.include?('Mode: 1')
+          puts 'Yes'
+        else
+          puts 'No'
+        end
+      end
+    end
 end
